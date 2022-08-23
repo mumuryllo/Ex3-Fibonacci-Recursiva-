d9805c3f0de98e487341a43c0800ef2f041e8611
Muryllo <mumuryllo>
2022-08-23 16:23:44 -0300

    Segunda leva ex5 recursividade

---
+++ Fibonacci/src/Controller.java
@@ -0,0 +1,12 @@
+
+public class Controller {
+
+   public static int fibonacci(int numero) {
+	   if(numero <2) {
+		   return 1;
+	   }
+	   return fibonacci(numero-1)+ fibonacci(numero-2);
+	   
+   }	
+	
+}
